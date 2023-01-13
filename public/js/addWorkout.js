b959d1d11dac98f5b438e9f7bd0609fe83a6bf1f
Jailene <jaileneveras8@gmail.com>
2023-01-13 18:48:25 -0500

    fixed typos to make showTextbox() work

--- public/js/addWorkout.js
+++ public/js/addWorkout.js
@@ -5,6 +5,8 @@ function showTextbox(answer){
     console.log(answer.value)
     if(answer.value == "Strength"){
        document.getElementById("strengthForm").classList.remove('s-none')
-    } else { document.getElementById("cardioForm").classList.remove('c-none')
+    } else { document.getElementById("CardioForm").classList.remove('c-none')
 }}
     
+
+showTextbox();
