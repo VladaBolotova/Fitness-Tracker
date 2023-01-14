162e2360e792dba537407dd8ea42f561bfdb48b2
Jailene <jaileneveras8@gmail.com>
2023-01-14 06:08:08 -0500

    updated css so user input can be seen without having to hover over textbox

--- public/js/addWorkout.js
+++ public/js/addWorkout.js
@@ -12,3 +12,29 @@ function showTextbox(answer){
     
 
 showTextbox();
+
+
+let workouts = [];
+        const addWorkout = (ev)=>{
+            ev.preventDefault();  //to stop the form submitting
+            let workout_deats = {
+                id: getElementById("date").value,
+                description: document.getElementById('desc').value,
+                weight: document.getElementById('weight').value,
+                sets: document.getElementById("sets").value,
+                reps:document.getElementById("reps").value,
+                calories:document.getElementById("cal").value,
+                Cardio:document.getElementById("desc_cardio").value,
+                duration: document.getElementById("duration").value
+            }
+            workouts.push(workout_deats);
+            console.log(workouts)
+            document.forms[0].reset();
+            localStorage.setItem('MyworkoutList', JSON.stringify(workouts) );
+
+            console.log(workouts)
+        }
+        document.addEventListener('DOMContentLoaded', ()=>{
+            document.getElementById('save-btn').addEventListener('click', addWorkout);
+        
+        });
