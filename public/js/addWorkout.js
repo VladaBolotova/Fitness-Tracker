5ac8f5021e4ace023e64eb11f21885d6c607bc07
Jailene <jaileneveras8@gmail.com>
2023-01-13 19:01:09 -0500

    added two lines to showtextbox function

--- public/js/addWorkout.js
+++ public/js/addWorkout.js
@@ -5,7 +5,9 @@ function showTextbox(answer){
     console.log(answer.value)
     if(answer.value == "Strength"){
        document.getElementById("strengthForm").classList.remove('s-none')
+       document.getElementById("CardioForm").classList.add('c-none')
     } else { document.getElementById("CardioForm").classList.remove('c-none')
+    document.getElementById("strengthForm").classList.add('s-none')
 }}
     
 
