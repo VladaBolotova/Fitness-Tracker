288818e09735d14c939a2543acbf72f8d42fdfb7
VladaBolotova <vladislavabolotova@gmail.com>
2023-01-18 16:58:36 -0500

    add workout js

---
+++ public/js/addWorkout.js
@@ -0,0 +1,8 @@
+const submitFormHandler = async(event)=> {
+    event.preventDefault();
+
+    const date = document.querySelector('').value.trim();
+    const duration = document.querySelector('').value.trim();
+
+    const type = document.querySelector('').value.trim();
+    const strengh = document.querySelector('').value.trim();
